e902d0b758d5ead9a47725230750ca7dd7872fe5
softRahul10 <softrahul10@gmail.com>
2023-06-04 23:42:40 +0530

    Code base added

---
+++ Routing/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/random/:min/:max', (req,res) => {
+      let minNumber = parseInt(req.params.min);
+      let maxNumber = parseInt(req.params.max);
+
+      if(isNaN(minNumber) || isNaN(maxNumber)) {
+            res.status(400);
+            res.json({error:"Bad Request"})
+            return;
+      }
+
+      let result = Math.round((Math.random() * (maxNumber - minNumber)) + minNumber); 
+      res.json({ result: result }); 
+
+});
+
+app.listen(3000,() => {
+      console.log('Server is Running!')
+})
